6b365a2b1caafbcabe1cc789f4ccee55328610c3
andrewliaskovski <andrewliaskovski@gmail.com>
2018-06-04 12:11:05 +0200

    Create logrotate.sh

---
+++ logrotate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+logdir1=/opt/log
+logdir2=/var/logs/tomcat
+logdir3=/var/log/apache2
+
+# find all files older than x days and gzip.
+# the backuped file is removed by gzip
+find $logdir1 $logdir2 -maxdepth 1 -type f -name "*.log" -mmin +1440 -print -exec gzip -q {} \;
+find $logdir1 $logdir2 -maxdepth 1 -type f -name "*.txt" -mmin +1440 -print -exec gzip -q {} \;
+find $logdir1 $logdir2 $logdir3 -maxdepth 1 -type f -name "*log*" -mmin +1440 ! -name "*gz*" -print -exec gzip -q {} \;
+find $logdir1 $logdir2 -maxdepth 2 -type f -name "*default*" -mmin +1440 ! -name "*gz*" -print -exec gzip -q {} \;
+
+###delete .gz files older than x days
+find $logdir1 $logdir2 $logdir3 -maxdepth 1 -type f \( -name '*.gz' \)  -mtime +7 -delete
