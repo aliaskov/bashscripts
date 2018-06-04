f6a10ce12eee934027be80c2b1a7d23c47f551a0
andrewliaskovski <andrewliaskovski@gmail.com>
2018-06-04 12:19:41 +0200

    Update logrotate.sh
    show all possible logs for rotating

--- logrotate.sh
+++ logrotate.sh
@@ -4,6 +4,9 @@ logdir1=/opt/log
 logdir2=/var/logs/tomcat
 logdir3=/var/log/apache2
 
+
+# find all files older than x days
+# find $logdir1 $logdir2 $logdir3 -maxdepth 1 -type f -name "*log*" -mmin +1440 ! -name "*gz*" -exec ls -lath {} \;
 # find all files older than x days and gzip.
 # the backuped file is removed by gzip
 find $logdir1 $logdir2 -maxdepth 1 -type f -name "*.log" -mmin +1440 -print -exec gzip -q {} \;
