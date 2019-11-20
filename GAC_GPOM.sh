03a70b2028abaf4faeacb8b986d454509a05fa18
Aliaskov <andrewliaskovski@gmail.com>
2019-11-20 18:45:17 +0100

    in case git was Initialized

--- GAC_GPOM.sh
+++ GAC_GPOM.sh
@@ -4,25 +4,28 @@ REPONAME=testrepo
 
 
 if [ ! -d $REPONAME ]; then
-echo "# testrepo" >> README.md
+mkdir $REPONAME 
+fi
+cd $REPONAME
+if [ ! -d .ssh ]; then
 git init
+git remote add origin $GITHUBREPO
+fi
+echo "# testrepo" >> README.md
 git add .
 git commit -m "initial commit"
-git remote add origin $GITHUBREPO
 git push -u origin master
 ## git push https://username:password@myrepository.biz/file.git --all 
-fi
-
 
 
 
 for run in {1..10}
 do
   sleep 1
-###let's make a dummy commit+push
+###let's make a dummy commit
   date >> README.md
   git add .
   git commit -m "test commit $run"
   git status
-  git push
 done
+git push
