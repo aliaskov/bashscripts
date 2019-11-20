6033f6a7f2306044f5df295a74098fbbecea49c0
Aliaskov <andrewliaskovski@gmail.com>
2019-11-20 19:45:57 +0100

    added user and password vars

--- GAC_GPOM.sh
+++ GAC_GPOM.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 GITHUBREPO=https://github.com/aliaskov/testrepo.git
-REPONAME=testrepo
+REPONAME=/tmp/testrepo
+GITHUB_USER=username
+GITHUB_PASSWORD=12234
 
 
+#in case if there is no directory
 if [ ! -d $REPONAME ]; then
 mkdir $REPONAME 
 fi
+
 cd $REPONAME
-if [ ! -d .ssh ]; then
+
+#if git is not initialized
+if [ ! -d .git ]; then
 git init
 git remote add origin $GITHUBREPO
 fi
+
 echo "# testrepo" >> README.md
 git add .
 git commit -m "initial commit"
 git push -u origin master
-## git push https://username:password@myrepository.biz/file.git --all 
+## git push https://$GITHUB_USER:$GITHUB_PASSWORD@myrepository.biz/file.git --all 
 
 
 
