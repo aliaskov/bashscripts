38d4e5e257aa63d3f51b36c9ef5e79fbb2835800
andrewliaskovski <andrewliaskovski@gmail.com>
2021-07-02 17:18:57 +0200

    Update DBBackup.sh

--- DBBackup.sh
+++ DBBackup.sh
@@ -7,10 +7,11 @@ databases=`mysql -u $USER -p$PASSWORD -h $HOST --silent  -e "SHOW DATABASES;" | 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD -h $HOST --single-transaction --column-statistics=0 --order-by-primary --compress  --databases $db > `date +%Y%m%d`.$db.sql
+        mysqldump -u $USER -p$PASSWORD -h $HOST --single-transaction  --order-by-primary --compress  --databases $db > `date +%Y%m%d`.$db.sql
+       # mysqldump -u $USER -p$PASSWORD -h $HOST --single-transaction --column-statistics=0 --order-by-primary --compress  --databases $db > `date +%Y%m%d`.$db.sql
     fi
 done
 ### Backup users and privileges
-mysql -u $USER -p$PASSWORD -h $HOST --skip-column-names -A -e "SELECT CONCAT('CREATE USER ','\'',user,'\'@\'',host,'\'','  IDENTIFIED BY PASSWORD ','\'',password,'\';') from mysql.user;" > Users.sql
+mysql -u $USER -p$PASSWORD -h $HOST --skip-column-names -A -e "SELECT CONCAT('CREATE USER ','\'',user,'\'@\'',host,'\'','  IDENTIFIED BY PASSWORD ','\'',authentication_string,'\';') from mysql.user;" > Users.sql
 mysql -u $USER -p$PASSWORD -h $HOST --skip-column-names -A -e "SELECT CONCAT('SHOW GRANTS FOR ''',user,'''@''',host,''';') AS query FROM mysql.user WHERE user NOT IN ('root','pma','rdsadmin','mysql.sys')" | mysql -u $USER -p$PASSWORD -h $HOST --skip-column-names -A | sed 's/$/;/g' | sed 's/IDENTIFIED BY PASSWORD <secret>//g' >> Users.sql
 echo "flush privileges;" >> Users.sql
