0ac87c8ac0cbf7673824be57b4ef62a520bb375a
Bugaienko <sergiibugaienko@gmail.com>
2023-06-15 20:33:49 +0200

    test2

--- dumpall.sh
+++ dumpall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 #30 1 * * * /opt/mysql/dumpall.sh > /var/backup/mysql/logs/dumpall_$(date +\%d-\%m-\%Y).log 2>&1
 #Hallo, Sergei Bugaenko was here
+#Test2 
 
 DBLIST=`echo "show databases;"|/usr/bin/mysql -s -uroot -pPASSWORD`
 LOCK=/tmp/mysql_dump.lck
